025b3bc4fd9dcacf26344d425a482633bff0b3c0
unknown <mimengis@microsoft.com>
2018-05-25 09:39:30 -0700

    Moving this to src

--- test/Microsoft.AspNetCore.SignalR.Common.Tests/HubLifetimeManagerTestsBase.cs
+++ test/Microsoft.AspNetCore.SignalR.Common.Tests/HubLifetimeManagerTestsBase.cs
@@ -1,142 +1,142 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+﻿//// Copyright (c) .NET Foundation. All rights reserved.
+//// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.SignalR.Protocol;
-using Microsoft.AspNetCore.SignalR.Tests;
-using Xunit;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.SignalR.Protocol;
+//using Microsoft.AspNetCore.SignalR.Tests;
+//using Xunit;
 
-namespace Microsoft.AspNetCore.SignalR.Common.Tests
-{
-    public abstract class HubLifeTimeManagerTestsBase<THub> where THub : Hub
-    {
-        public HubLifetimeManager<THub> Manager;
+//namespace Microsoft.AspNetCore.SignalR.Common.Tests
+//{
+//    public abstract class HubLifeTimeManagerTestsBase<THub> where THub : Hub
+//    {
+//        public HubLifetimeManager<THub> Manager;
 
-        public abstract HubLifetimeManager<THub> GetNewHubLifetimeManager();
+//        public abstract HubLifetimeManager<THub> GetNewHubLifetimeManager();
 
-        [Fact]
-        public async Task SendAllAsyncWritesToAllConnectionsOutput()
-        {
-            using (var client1 = new TestClient())
-            using (var client2 = new TestClient())
-            {
-                var manager = GetNewHubLifetimeManager();
-                var connection1 = HubConnectionContextUtils.Create(client1.Connection);
-                var connection2 = HubConnectionContextUtils.Create(client2.Connection);
+//        [Fact]
+//        public async Task SendAllAsyncWritesToAllConnectionsOutput()
+//        {
+//            using (var client1 = new TestClient())
+//            using (var client2 = new TestClient())
+//            {
+//                var manager = GetNewHubLifetimeManager();
+//                var connection1 = HubConnectionContextUtils.Create(client1.Connection);
+//                var connection2 = HubConnectionContextUtils.Create(client2.Connection);
 
-                await manager.OnConnectedAsync(connection1).OrTimeout();
-                await manager.OnConnectedAsync(connection2).OrTimeout();
+//                await manager.OnConnectedAsync(connection1).OrTimeout();
+//                await manager.OnConnectedAsync(connection2).OrTimeout();
 
-                await manager.SendAllAsync("Hello", new object[] { "World" }).OrTimeout();
+//                await manager.SendAllAsync("Hello", new object[] { "World" }).OrTimeout();
 
-                var message = Assert.IsType<InvocationMessage>(client1.TryRead());
-                Assert.Equal("Hello", message.Target);
-                Assert.Single(message.Arguments);
-                Assert.Equal("World", (string)message.Arguments[0]);
+//                var message = Assert.IsType<InvocationMessage>(client1.TryRead());
+//                Assert.Equal("Hello", message.Target);
+//                Assert.Single(message.Arguments);
+//                Assert.Equal("World", (string)message.Arguments[0]);
 
-                message = Assert.IsType<InvocationMessage>(client2.TryRead());
-                Assert.Equal("Hello", message.Target);
-                Assert.Single(message.Arguments);
-                Assert.Equal("World", (string)message.Arguments[0]);
-            }
-        }
+//                message = Assert.IsType<InvocationMessage>(client2.TryRead());
+//                Assert.Equal("Hello", message.Target);
+//                Assert.Single(message.Arguments);
+//                Assert.Equal("World", (string)message.Arguments[0]);
+//            }
+//        }
 
-        [Fact]
-        public async Task SendAllAsyncDoesNotWriteToDisconnectedConnectionsOutput()
-        {
-            using (var client1 = new TestClient())
-            using (var client2 = new TestClient())
-            {
-                var manager = GetNewHubLifetimeManager();
-                var connection1 = HubConnectionContextUtils.Create(client1.Connection);
-                var connection2 = HubConnectionContextUtils.Create(client2.Connection);
+//        [Fact]
+//        public async Task SendAllAsyncDoesNotWriteToDisconnectedConnectionsOutput()
+//        {
+//            using (var client1 = new TestClient())
+//            using (var client2 = new TestClient())
+//            {
+//                var manager = GetNewHubLifetimeManager();
+//                var connection1 = HubConnectionContextUtils.Create(client1.Connection);
+//                var connection2 = HubConnectionContextUtils.Create(client2.Connection);
 
-                await manager.OnConnectedAsync(connection1).OrTimeout();
-                await manager.OnConnectedAsync(connection2).OrTimeout();
+//                await manager.OnConnectedAsync(connection1).OrTimeout();
+//                await manager.OnConnectedAsync(connection2).OrTimeout();
 
-                await manager.OnDisconnectedAsync(connection2).OrTimeout();
+//                await manager.OnDisconnectedAsync(connection2).OrTimeout();
 
-                await manager.SendAllAsync("Hello", new object[] { "World" }).OrTimeout();
+//                await manager.SendAllAsync("Hello", new object[] { "World" }).OrTimeout();
 
-                var message = Assert.IsType<InvocationMessage>(client1.TryRead());
-                Assert.Equal("Hello", message.Target);
-                Assert.Single(message.Arguments);
-                Assert.Equal("World", (string)message.Arguments[0]);
+//                var message = Assert.IsType<InvocationMessage>(client1.TryRead());
+//                Assert.Equal("Hello", message.Target);
+//                Assert.Single(message.Arguments);
+//                Assert.Equal("World", (string)message.Arguments[0]);
 
-                Assert.Null(client2.TryRead());
-            }
-        }
+//                Assert.Null(client2.TryRead());
+//            }
+//        }
 
-        [Fact]
-        public async Task SendGroupAsyncWritesToAllConnectionsInGroupOutput()
-        {
-            using (var client1 = new TestClient())
-            using (var client2 = new TestClient())
-            {
-                var manager = GetNewHubLifetimeManager();
-                var connection1 = HubConnectionContextUtils.Create(client1.Connection);
-                var connection2 = HubConnectionContextUtils.Create(client2.Connection);
+//        [Fact]
+//        public async Task SendGroupAsyncWritesToAllConnectionsInGroupOutput()
+//        {
+//            using (var client1 = new TestClient())
+//            using (var client2 = new TestClient())
+//            {
+//                var manager = GetNewHubLifetimeManager();
+//                var connection1 = HubConnectionContextUtils.Create(client1.Connection);
+//                var connection2 = HubConnectionContextUtils.Create(client2.Connection);
 
-                await manager.OnConnectedAsync(connection1).OrTimeout();
-                await manager.OnConnectedAsync(connection2).OrTimeout();
+//                await manager.OnConnectedAsync(connection1).OrTimeout();
+//                await manager.OnConnectedAsync(connection2).OrTimeout();
 
-                await manager.AddToGroupAsync(connection1.ConnectionId, "gunit").OrTimeout();
+//                await manager.AddToGroupAsync(connection1.ConnectionId, "gunit").OrTimeout();
 
-                await manager.SendGroupAsync("gunit", "Hello", new object[] { "World" }).OrTimeout();
+//                await manager.SendGroupAsync("gunit", "Hello", new object[] { "World" }).OrTimeout();
 
-                var message = Assert.IsType<InvocationMessage>(client1.TryRead());
-                Assert.Equal("Hello", message.Target);
-                Assert.Single(message.Arguments);
-                Assert.Equal("World", (string)message.Arguments[0]);
+//                var message = Assert.IsType<InvocationMessage>(client1.TryRead());
+//                Assert.Equal("Hello", message.Target);
+//                Assert.Single(message.Arguments);
+//                Assert.Equal("World", (string)message.Arguments[0]);
 
-                Assert.Null(client2.TryRead());
-            }
-        }
+//                Assert.Null(client2.TryRead());
+//            }
+//        }
 
-        [Fact]
-        public async Task SendGroupExceptAsyncDoesNotWriteToExcludedConnections()
-        {
-            using (var client1 = new TestClient())
-            using (var client2 = new TestClient())
-            {
-                var manager = GetNewHubLifetimeManager();
-                var connection1 = HubConnectionContextUtils.Create(client1.Connection);
-                var connection2 = HubConnectionContextUtils.Create(client2.Connection);
+//        [Fact]
+//        public async Task SendGroupExceptAsyncDoesNotWriteToExcludedConnections()
+//        {
+//            using (var client1 = new TestClient())
+//            using (var client2 = new TestClient())
+//            {
+//                var manager = GetNewHubLifetimeManager();
+//                var connection1 = HubConnectionContextUtils.Create(client1.Connection);
+//                var connection2 = HubConnectionContextUtils.Create(client2.Connection);
 
-                await manager.OnConnectedAsync(connection1).OrTimeout();
-                await manager.OnConnectedAsync(connection2).OrTimeout();
+//                await manager.OnConnectedAsync(connection1).OrTimeout();
+//                await manager.OnConnectedAsync(connection2).OrTimeout();
 
-                await manager.AddToGroupAsync(connection1.ConnectionId, "gunit").OrTimeout();
-                await manager.AddToGroupAsync(connection2.ConnectionId, "gunit").OrTimeout();
+//                await manager.AddToGroupAsync(connection1.ConnectionId, "gunit").OrTimeout();
+//                await manager.AddToGroupAsync(connection2.ConnectionId, "gunit").OrTimeout();
 
-                await manager.SendGroupExceptAsync("gunit", "Hello", new object[] { "World" }, new[] { connection2.ConnectionId }).OrTimeout();
+//                await manager.SendGroupExceptAsync("gunit", "Hello", new object[] { "World" }, new[] { connection2.ConnectionId }).OrTimeout();
 
-                var message = Assert.IsType<InvocationMessage>(client1.TryRead());
-                Assert.Equal("Hello", message.Target);
-                Assert.Single(message.Arguments);
-                Assert.Equal("World", (string)message.Arguments[0]);
+//                var message = Assert.IsType<InvocationMessage>(client1.TryRead());
+//                Assert.Equal("Hello", message.Target);
+//                Assert.Single(message.Arguments);
+//                Assert.Equal("World", (string)message.Arguments[0]);
 
-                Assert.Null(client2.TryRead());
-            }
-        }
+//                Assert.Null(client2.TryRead());
+//            }
+//        }
 
-        [Fact]
-        public async Task SendConnectionAsyncWritesToConnectionOutput()
-        {
-            using (var client = new TestClient())
-            {
-                var manager = GetNewHubLifetimeManager();
-                var connection = HubConnectionContextUtils.Create(client.Connection);
+//        [Fact]
+//        public async Task SendConnectionAsyncWritesToConnectionOutput()
+//        {
+//            using (var client = new TestClient())
+//            {
+//                var manager = GetNewHubLifetimeManager();
+//                var connection = HubConnectionContextUtils.Create(client.Connection);
 
-                await manager.OnConnectedAsync(connection).OrTimeout();
+//                await manager.OnConnectedAsync(connection).OrTimeout();
 
-                await manager.SendConnectionAsync(connection.ConnectionId, "Hello", new object[] { "World" }).OrTimeout();
+//                await manager.SendConnectionAsync(connection.ConnectionId, "Hello", new object[] { "World" }).OrTimeout();
 
-                var message = Assert.IsType<InvocationMessage>(client.TryRead());
-                Assert.Equal("Hello", message.Target);
-                Assert.Single(message.Arguments);
-                Assert.Equal("World", (string)message.Arguments[0]);
-            }
-        }
-    }
-}
+//                var message = Assert.IsType<InvocationMessage>(client.TryRead());
+//                Assert.Equal("Hello", message.Target);
+//                Assert.Single(message.Arguments);
+//                Assert.Equal("World", (string)message.Arguments[0]);
+//            }
+//        }
+//    }
+//}
