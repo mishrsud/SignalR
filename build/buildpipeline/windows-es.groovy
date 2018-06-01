532ecc37a792b5c355a86f8eb57e7db5529fbf58
BrennanConroy <brecon@microsoft.com>
2018-06-01 11:50:37 -0700

    lets try

--- build/buildpipeline/windows-es.groovy
+++ build/buildpipeline/windows-es.groovy
@@ -2,7 +2,7 @@
 
 // 'node' indicates to Jenkins that the enclosed block runs on a node that matches
 // the label 'windows-with-vs'
-simpleNode('Windows.10.Amd64.ClientRS4.ES.Open') {
+simpleNode('Windows-10-Enterprise-RS4-ASPNET-es-ES-05-08-2018') {
     stage ('Checking out source') {
         checkout scm
     }
